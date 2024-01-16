b56633e9d2022e3f252bd79e836305e7ea85f355
Marvin Gith. <marvingithambo@gmail.com>
2024-01-16 09:55:02 +0300

    Basicsof-Javascript

---
+++ 1.basics.js
@@ -0,0 +1,30 @@
+//identifiers
+//Start with a letter( lowercase or uppercase), number, dollar sign, underscore.
+//camelCase
+let firstName = "Marvin"
+// snake case
+let second_name = "Githambo"
+//Pascla Case
+let BloodRatio = 32.78
+let $my_age = 30
+let _dob = 1996
+//let total = salary_one + salary_two
+let salary_one = 23000
+let salary_two = 25000
+let total = salary_one + salary_two
+console.log(total)
+//Assignment Operators
+let i = 12
+i++
+console.log(i)
+++i
+console.log(i)
+// Assignment Operators
+let a = 1
+let b = 2
+a += b 
+console.log(a)//3
+
+//% reminder
+console.log(9%2)
+
---
+++ 2datatypes.js
@@ -0,0 +1,25 @@
+//String
+let my_name = "Marvin Githambo"
+
+//number
+let my_age = 40
+
+//boolean
+let I_am_white = false
+let I_am_black = true
+
+//Undefined
+let studentMarks;
+console.log(studentMarks)
+//Null
+let student_age = ''
+console.log(student_age)
+
+// Example of an object -holds data
+let studentInfo = {
+    //key:value
+    studentID:2304924,
+    studentName : 'Marvin',
+    student_age:34
+}
+console.log(studentInfo)
